f1bb3a86580cb2ef7b624aed0670c7878df55c94
ellodikia <gabriellaodikia@gmail.com>
2025-08-27 18:32:54 +0700

    update login 1

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,12 @@
 <?php
+
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "db_loginmultiuser";
+$db   = "db_loginmultiuser";
 
-$koneksi = new mysqli($host, $user, $pass, $db);
+$koneksi = new mysqli ($host, $user, $pass, $db);
 
 if ($koneksi->connect_error) {
-    die("Koneksi gagal: " . $koneksi->connect_error);
+    die ("Koneksi gagal: ". $koneksi->connect_error);
 }
--- login.php
+++ login.php
@@ -1,36 +1,34 @@
 <?php
-
 session_start();
-include'koneksi.php';
+include 'koneksi.php';
 
 $error = "";
-if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
-    $username =  trim ($_POST ['username']);
-    $password =  $_POST ['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
 
-    $stmt = $koneksi->prepare("SELECT * FROM user WHERE username=? AND password=?");
-    $stmt->bind_param("ss", $username ,$password);
+    $stmt = $koneksi->prepare("SELECT * FROM users WHERE username=? AND password=?");
+    $stmt->bind_param("ss", $username, $password);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result->num_rows ===1) {
-        $num = $result->fetch_assoc();
-        $_SESSION['username'] =$row['username'];
-        $_SESSION['level']    =$row['level'];
+    if ($result->num_rows === 1) {
+        $rows = $result->fetch_assoc();
+        $_SESSION['username'] = $rows['username'];
+        $_SESSION['level'] = $rows['level'];
 
-        if ($row ['level'] == "admin") {
-            header ("Location: dashboard_admin.php");
-        } elseif ($row ['level'] == "user") {
-            header ("Location: dashboard_user.php");
-        } else { 
-            $error = "Level tidak dikenali!";
+        if ($rows['level'] == "admin") {
+            header("Location: dashboard_admin.php");
+        } elseif ($rows['level'] == "user") {
+            header("Location: dashboard_user.php");
+        } else {
+            $error = "Level tidak dikenali";
         }
         exit();
     } else {
         $error = "Username atau password salah!";
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -38,14 +36,14 @@ if ($_SERVER ['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
+    <title>Login</title>
 </head>
 <body>
     <form action="" method="post">
         <div class="login">
             <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" values="Login">
+            <input type="password" name="passwod" placeholder="Password" required>
+            <input type="submit" value="Login">
         </div>
     </form>
 </body>
