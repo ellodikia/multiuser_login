59be980d06a2d2ecf0cf35421c85d35c6098fcca
ellodikia <gabriellaodikia@gmail.com>
2025-08-27 22:36:43 +0700

    Update login 2

--- login.php
+++ login.php
@@ -1,34 +1,38 @@
 <?php
+
 session_start();
 include 'koneksi.php';
 
 $error = "";
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = trim($_POST['username']);
-    $password = $_POST['password'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+$username = trim ($_POST['username']);
+$password = $_POST['password'];
 
-    $stmt = $koneksi->prepare("SELECT * FROM users WHERE username=? AND password=?");
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
+$stmt = $koneksi->prepare("SELECT * FROM users WHERE username=? AND password=?");
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+$result = $stmt->get_result();
 
-    if ($result->num_rows === 1) {
-        $rows = $result->fetch_assoc();
-        $_SESSION['username'] = $rows['username'];
-        $_SESSION['level'] = $rows['level'];
+if ($result->num_rows ===1) {
+    $row = $result->fetch_assoc();
+    $_SESSION ['username'] =$row ['username'];
+    $_SESSION ['level'] =$row ['level'];
 
-        if ($rows['level'] == "admin") {
-            header("Location: dashboard_admin.php");
-        } elseif ($rows['level'] == "user") {
-            header("Location: dashboard_user.php");
-        } else {
-            $error = "Level tidak dikenali";
-        }
-        exit();
+if ($row ['level'] = "admin") {
+    header ("Location: dashboard_admin.php");
+} elseif  ($row ['level'] = "user") {
+    header ("Location: dashboard_user.php");
     } else {
-        $error = "Username atau password salah!";
+        $error = "Level tidak dikenali";
     }
+    exit();
 }
+else{
+    $error = "Username atau password salah";
+}
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <form action="" method="post">
         <div class="login">
             <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="passwod" placeholder="Password" required>
+            <input type="password" name="password" placeholder="Password" required>
             <input type="submit" value="Login">
         </div>
     </form>
